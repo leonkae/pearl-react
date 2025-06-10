9fd51158d2b92864f9b567e7e75d95a3fbe9c5dc
leonkae <leon29kae@gmail.com>
2025-06-10 11:42:35 +0300

    components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import { Fragment } from "react/jsx-runtime";
 import Nav from "./components/navComponent.js";
 import FooterComponent from "./components/footerComponent.js";
-
+import Bout from "./components/boutComponent.js";
+import Button from "./components/buttonComponent.js";
 // This code imports React and the necessary components and styles for the application.
 // It defines a functional component `App` that renders a navigation bar, a title, and a subtitle using JSX syntax. 
 
@@ -17,9 +18,12 @@ function App() {
       <Nav/>
       {title}
       {subtitle}
+      <Button/>
       <h1>Title is </h1>
       <h2>My name is {name}</h2>
+      <Bout/>
 <FooterComponent/>
+
     </div>
     
   );
---
+++ src/components/boutComponent.js
@@ -0,0 +1,9 @@
+function Bout() {
+    return(
+        <div>
+            <p>this is the bout</p>
+        </div>
+    )
+    
+}
+export default Bout;
